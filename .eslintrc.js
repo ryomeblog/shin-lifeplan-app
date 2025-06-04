17ecab0b2f59d423dc8c99ee8f889c6b52047d82
ryome <ryome.blog@gmail.com>
2025-06-05 00:00:19 +0900

    コードのリンティングとフォーマットにESLintとPrettierを追加しました。
    - package.jsonにリンティングスクリプト「lint」、「lint:fix」、「format」を追加しました。
    - 必要なプラグインと設定とともに、ESLintとPrettierをdevDependenciesとして追加しました。

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react', 'react-hooks', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
